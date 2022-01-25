578c25d7bc8194f1efa96fdc65c13a221bd121ed
jpunkt <johannes@arg-art.org>
2022-01-25 20:20:58 +0100

    removed move parameter from statemachine_test

--- pizzactrl/statemachine_test.py
+++ pizzactrl/statemachine_test.py
@@ -6,10 +6,9 @@ logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 from pizzactrl.statemachine import Statemachine
 # from pizzactrl.sb_dummy import STORYBOARD
-from pizzactrl.sb_showcase import STORYBOARD
+from pizzactrl.sb_dummy import STORYBOARD
 from pizzactrl.hal_serial import PizzaHAL, rewind, turn_off
 
 hal = PizzaHAL()
-# sm = Statemachine(hal, STORYBOARD, move=True, loop=False, test=True)
 
-sm = Statemachine(hal, STORYBOARD, move=True, loop=False, test=False)
+sm = Statemachine(hal, STORYBOARD, loop=False, test=False)

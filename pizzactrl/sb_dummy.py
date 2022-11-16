1f887abdd901c5a94ac2ef3ab68d2252dc63ab6d
jpunkt <johannes@arg-art.org>
2022-11-16 18:35:52 +0100

    Testing video postprocessing (passed)

--- pizzactrl/sb_dummy.py
+++ pizzactrl/sb_dummy.py
@@ -3,13 +3,48 @@ from pizzactrl import fs_names
 from pizzactrl.storyboard import *
 
 STORYBOARD = Storyboard(
+    # Chapter(
+    #     Do(Activity.WAIT_FOR_INPUT,
+    #         on_blue=Select(Option.CONTINUE))
+    # ),
+    Chapter(
+        Do(Activity.PLAY_SOUND,
+            sound=fs_names.SFX_REC_AUDIO),
+        Do(Activity.RECORD_SOUND,
+            filename=fs_names.RecFile('my_audio.wav'),
+            cache=True,
+            duration=10),
+        Do(Activity.PLAY_SOUND,
+            sound=fs_names.SFX_STOP_REC)
+    ),
     Chapter(
         Do(Activity.PLAY_SOUND,
             sound=fs_names.SFX_REC_AUDIO),
         Do(Activity.RECORD_VIDEO,
             filename=fs_names.RecFile('my_video.h264'),
+            sound=fs_names.RecFile('my_audio.wav'),
+            duration=5),
+        Do(Activity.PLAY_SOUND,
+            sound=fs_names.SFX_STOP_REC)
+    ),
+    Chapter(
+        Do(Activity.PLAY_SOUND,
+            sound=fs_names.SFX_REC_AUDIO),
+        Do(Activity.RECORD_SOUND,
+            filename=fs_names.RecFile('my_audio2.wav'),
+            cache=True,
             duration=10),
         Do(Activity.PLAY_SOUND,
             sound=fs_names.SFX_STOP_REC)
-    )
+    ),
+    # Chapter(
+    #     Do(Activity.PLAY_SOUND,
+    #         sound=fs_names.SFX_REC_AUDIO),
+    #     Do(Activity.RECORD_VIDEO,
+    #         filename=fs_names.RecFile('my_video2.h264'),
+    #         sound=fs_names.RecFile('my_audio2.wav'),
+    #         duration=5),
+    #     Do(Activity.PLAY_SOUND,
+    #         sound=fs_names.SFX_STOP_REC)
+    # )
 )
--- pizzactrl/statemachine_test.py
+++ pizzactrl/statemachine_test.py
@@ -5,10 +5,10 @@ import logging
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 from pizzactrl.statemachine import Statemachine, State
-# from pizzactrl.sb_dummy import STORYBOARD
-from pizzactrl.sb_linz import STORYBOARD
+from pizzactrl.sb_dummy import STORYBOARD
+# from pizzactrl.sb_linz import STORYBOARD
 from pizzactrl.hal_serial import PizzaHAL, rewind, turn_off, reset
 
 hal = PizzaHAL()
 
-sm = Statemachine(hal, STORYBOARD, loop=False, test=False, lang_select=False)
+sm = Statemachine(hal, STORYBOARD, loop=False, test=False, lang_select=True)
